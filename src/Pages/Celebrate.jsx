77469f7ccb2cb7b52b8042ca0a20190244d8e369
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-02-03 16:40:59 +0800

    upate

---
+++ src/Pages/Celebrate.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Celebrate() {
+    return (
+        <div className='h-screen flex flex-col items-center justify-center'>
+            <h1 className='font-semibold'>Horaaay Date na Tayooo~</h1>
+        </div>
+    )
+}
+
+export default Celebrate
