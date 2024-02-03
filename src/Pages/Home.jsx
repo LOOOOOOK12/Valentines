6730a7ee24fc1da6d5a1308d6c820b5850bb2b04
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-02-03 16:41:02 +0800

    upate

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,33 +1,31 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+    const navigate = useNavigate();
     const [name, setName] = useState("");
 
     const handleInputChange = (event) => {
         setName(event.target.value);
     };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-    };
-
     return (
         <div className="flex flex-col justify-center items-center h-screen ">
-            <form
-                className="flex flex-col justify-center items-center"
-                onSubmit={handleSubmit}
-            >
-                <label>What is your name:</label>
+            <div className="flex flex-col justify-center items-center gap-5">
+                <h1 className="text-3xl font-semibold">What is your name?</h1>
                 <input
+                    className="p-2 border-slate-900 border-2 rounded-md"
                     type="text"
-                    value={name}
+                    name='name'
                     onChange={handleInputChange}
                 />
-                <Link to={`/Question?name=${encodeURIComponent(name)}`}>
-                    <button type="submit">Okay</button>
-                </Link>
-            </form>
+                <button 
+                    className="px-5 py-3 rounded-md bg-green-600 text-slate-200" 
+                    onClick={()=>{navigate('/Question', {replace: true, state:{name}})}}
+                >
+                    Okay
+                </button>
+            </div>
         </div>
     );
 }
