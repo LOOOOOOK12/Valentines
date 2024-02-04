99e2a15339d369f44a9b5362fdcb7711157de9be
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-02-04 19:22:04 +0800

    update

--- src/Pages/Celebrate.jsx
+++ src/Pages/Celebrate.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import { Link } from "react-router-dom"
+import cina from "../assets/cina.gif"
 
 function Celebrate() {
     return (
-        <div className='h-screen flex flex-col items-center justify-center'>
-            <h1 className='font-semibold'>Horaaay Date na Tayooo~</h1>
+        <div className='h-screen flex flex-col items-center justify-center gap-6 bg-[#65ccff]'>
+            <img src={cina} className="h-[20rem] mb-6 rounded-lg" />
+            <h1 className="text-4xl font-semibold text-zinc-50">Horaaay Date na Tayooo~</h1>
+            <Link to='/'><button className="px-5 py-3 rounded-md bg-blue-700 text-slate-200">Back</button></Link>
         </div>
     )
 }
