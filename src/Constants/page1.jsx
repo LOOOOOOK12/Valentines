80357eefe48490273ddf5137fdff303ef5adf9b3
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-02-05 18:56:22 +0800

    update

---
+++ src/Constants/page1.jsx
@@ -0,0 +1,23 @@
+import gif1 from '../assets/cinnamon.gif'
+import gif2 from '../assets/cina.gif'
+import gif3 from '../assets/cinnamoroll-wave.gif'
+import gif4 from '../assets/oh-no-cinnamoroll.gif'
+
+export const page1 = [
+    {
+        gif: gif1,
+        alt: "cinnamon"
+    },
+    {
+        gif: gif2,
+        alt: "cinnamon"
+    },
+    {
+        gif: gif3,
+        alt: "cinnamon"
+    },
+    {
+        gif: gif4,
+        alt: "cinnamon"
+    },
+]
