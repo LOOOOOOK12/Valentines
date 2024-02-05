2fce34d485b94b292edde397c7fba86207e33140
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-02-05 18:56:25 +0800

    update

---
+++ src/Constants/page2.jsx
@@ -0,0 +1,19 @@
+import gif1 from '../assets/cin1.gif'
+import gif2 from '../assets/cin2.gif'
+import gif3 from '../assets/cin3.gif'
+import gif4 from '../assets/cin4.gif'
+
+export const page1 = [
+    {
+        gif:gif1,
+    },
+    {
+        gif:gif2,
+    },
+    {
+        gif:gif3,
+    },
+    {
+        gif:gif4,
+    },
+]
