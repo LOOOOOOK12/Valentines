42ca4d2e30e5b0147c2a96f4bfb2cec97e8bc44e
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-02-05 18:56:36 +0800

    update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+
 function Home() {
     const navigate = useNavigate();
     const [name, setName] = useState("");
@@ -12,15 +13,15 @@ function Home() {
     return (
         <div className="flex flex-col justify-center items-center h-screen bg-[#65ccff]">
             <div className="flex flex-col justify-center items-center gap-5">
-                <h1 className="text-3xl font-semibold text-zinc-50">What is your name?</h1>
+                <h1 className="text-3xl font-semibold text-zinc-50">What's your name?</h1>
                 <input
-                    className="p-2 border-[#7dd4ff] border-2 rounded-md"
+                    className="p-2 border-none border-2 rounded-md"
                     type="text"
                     name='name'
                     onChange={handleInputChange}
                 />
                 <button 
-                    className="px-5 py-3 rounded-md bg-green-600 text-slate-200" 
+                    className="px-5 py-3 rounded-md bg-green-500 text-slate-100 hover:bg-green-700 ease-in-out duration-300" 
                     onClick={()=>{navigate('/Question', {replace: true, state:{name}})}}
                 >
                     Okay
